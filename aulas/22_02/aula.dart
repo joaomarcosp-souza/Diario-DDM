5bb1b66aa8058b9e5001987f614e33ed6dc613e6
João Marcos <112454993+joaomarcosp-souza@users.noreply.github.com>
2024-02-22 22:31:51 -0300

    Pasta para o diario - 22/02
    Atividade realizadas até a 04

---
+++ aulas/22_02/aula.dart
@@ -0,0 +1,80 @@
+void main() {
+  // print('variaveis');
+  // String nomeAluno = 'João';
+  // String sobrenomeAluno = 'Marcos';
+
+  // int idadeAluno = 24;
+  // idadeAluno = 20;
+  // idadeAluno = 25;
+
+  // double peso = 70.5;
+
+  // print(nomeAluno + sobrenomeAluno);
+
+  // print('${nomeAluno} ${sobrenomeAluno}'); //interpolação
+  // print('$nomeAluno $sobrenomeAluno'); //interpolação simples
+  // print(
+  //     '$nomeAluno ${idadeAluno >= 18 ? 'não precisa' : 'precisa'}'); // operador alternario
+
+  //Atividade 01
+  // String disciplina = 'Desenvolvimento Mobile';
+  // double cargaHoraria = 125.0;
+  // String professorResponsavel = 'Hélio';
+  // bool materiaAtiva = true;
+  // int quantidadeAlunos = 30;
+
+  // print(disciplina +
+  //     professorResponsavel +
+  //     ' ' +
+  //     '${cargaHoraria}' +
+  //     ' ' +
+  //     '$quantidadeAlunos' +
+  //     '${materiaAtiva == true ? 'ativada' : 'desativa'}');
+
+  // Atividade 02
+  // String cor = 'Preta';
+  // String tipo = 'Camisa';
+  // String TAMANHOROUPA = 'GG';
+  // double valor = 80.50;
+  // bool vendido = false;
+
+  // print("""
+  //     $cor
+  //     $tipo
+  //     $TAMANHOROUPA
+  //     $valor
+  //     $vendido
+  //   """);
+
+  //Atividade 03
+  // var disciplina;
+  // disciplina = 'Desenvolvimento Mobile';
+  // var cargaHoraria = 125.0;
+  // var professorResponsavel;
+  // professorResponsavel = 'Hélio';
+  // var materiaAtiva = true;
+  // var quantidadeAlunos = 30;
+
+  // // // Atividade 04
+  // dynamic cor = 'Preto';
+  // cor = 'Preto02';
+  // dynamic tipo = 'Camisa';
+  // dynamic TAMANHOROUPA = 'GG';
+  // dynamic valor = 80;
+  // valor = 80.50;
+  // dynamic vendido = false;
+  // vendido = 'vendeu';
+
+  // print("""
+  //   $cor
+  //   $tipo
+  //   $TAMANHOROUPA
+  //   $valor
+  //   $vendido
+  // """);
+
+  // ATIVIDADE final/const
+  const int valorVenda = 100;
+  const int valorDesconto = 50;
+  const int valorFinal = valorVenda - valorDesconto;
+}
